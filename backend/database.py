672b6fb600eb8141c8dc7b2ce0013758e4134391
Zubi <afshanzubia.akmal05@gmail.com>
2024-09-28 22:52:17 -0400

    changed database code

--- backend/database.py
+++ backend/database.py
@@ -1,35 +1,35 @@
 # To use database:
 # pip install pymongo
 
+#Only function of this class is to take response.json and store in a MongoDB database.
 from pymongo import MongoClient
 
 import fastapi2
 
-connection_string = "mongodb+srv://no89:<8pzd1hFpG5KFqPCK>@mydatabase.x3ahw.mongodb.net/?retryWrites=true&w=majority&appName=mydatabase"
+class DBCOnnection:
+    def __init__(self):
+        # Connect to MongoDB Atlas
+        # Specify the database and collection
+        self.connection_string = "mongodb+srv://no89:<8pzd1hFpG5KFqPCK>@mydatabase.x3ahw.mongodb.net/?retryWrites=true&w=majority&appName=mydatabase"
+        self.db = self.client("my_database")
+        self.collection  = self.db["users"]
+    
 
-# Connect to MongoDB Atlas
-client = MongoClient(connection_string)
+    def insert_a_user(response):
+        #resposne is sent from fastapi when it makes a call
+        resume = response
+        user_data = {
+            "name": resume.name,
+            "email": resume.email,
+            "resume text": resume.text
+            collection.insert_one(user_data)
+            print("Data successfully inserted!")
+            client.close()
+        }
 
-# Specify the database and collection
-db = client["my_database"]
-collection = db["users"]
+    def close(self):
+        self.client.close()
 
-# Get user input
-name = input("Enter your name: ")
-email = input("Enter your email: ")
-resume = response
 
-# Create a document to insert into MongoDB
-user_data = {
-    "name": name,
-    "email": email,
-    "resume": resume
-}
 
-# Insert the document into the collection
-collection.insert_one(user_data)
 
-print("Data successfully inserted!")
-
-# Close the connection
-client.close()
